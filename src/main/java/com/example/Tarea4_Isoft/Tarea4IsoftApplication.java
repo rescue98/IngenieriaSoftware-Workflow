30f97da75802b5ee794c218a802cd09622abf719
cony_moya <c.moya04@ufromail.cl>
2023-06-13 10:52:40 -0400

    Inicio Proyecto

---
+++ src/main/java/com/example/Tarea4_Isoft/Tarea4IsoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tarea4_Isoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea4IsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea4IsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tarea4_Isoft/Tarea4IsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tarea4_Isoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea4IsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
